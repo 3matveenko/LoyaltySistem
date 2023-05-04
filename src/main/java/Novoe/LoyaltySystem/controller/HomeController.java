535b5b7e2cff5b4ff632668b25aaf8d1d0b672e6
3matveenko <3matveenkokz@gmail.com>
2023-05-04 18:05:53 +0600

    тест коммит

--- src/main/java/Novoe/LoyaltySystem/controller/HomeController.java
+++ src/main/java/Novoe/LoyaltySystem/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+//
     @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/")
     public String index(){
