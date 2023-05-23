2b5ff12197c93608e10c8d8fbe36d8769fd9f104
3matveenko <3matveenkokz@gmail.com>
2023-05-23 18:04:10 +0600

    18.05

--- src/main/java/Novoe/LoyaltySystem/controller/CardController.java
+++ src/main/java/Novoe/LoyaltySystem/controller/CardController.java
@@ -4,8 +4,11 @@ import Novoe.LoyaltySystem.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
@@ -15,8 +18,22 @@ public class CardController {
     @Autowired
     CardService cardService;
 
-    @GetMapping(value = "/create")
-    public String create(){
+    @GetMapping(value = "/create/{companyId}")
+    public String create(
+            @PathVariable("companyId") Long companyId,
+            Model model){
+        model.addAttribute("id", companyId);
         return "card/create";
+    }
+
+    @PostMapping(value = "/add")
+    public String createCard(
+            Model model,
+            @RequestParam("id_company") Long idCompany,
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("typeOfDiscount") String typeOfDiscount) throws IOException {
+        cardService.createCard(file, name, typeOfDiscount, idCompany);
+        return "redirect:/company/details/"+idCompany;
     }
 }
--- src/main/java/Novoe/LoyaltySystem/service/CardService.java
+++ src/main/java/Novoe/LoyaltySystem/service/CardService.java
@@ -1,10 +1,22 @@
 package Novoe.LoyaltySystem.service;
 
+import Novoe.LoyaltySystem.model.Card;
 import Novoe.LoyaltySystem.model.Company;
 import Novoe.LoyaltySystem.repository.CardRepository;
 import Novoe.LoyaltySystem.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CardService {
@@ -20,5 +32,22 @@ public class CardService {
         return company.getCards().size();
     }
 
-
+    public String createCard(MultipartFile file, String name, String typeOfDiscount,Long idCompany) throws IOException{
+        String imagename = UUID.randomUUID().toString();
+        String fileName = imagename+".jpeg";
+        String destinationPath = Paths.get("src/main/resources/static/dist/img/cards").toString();
+        Path targetPath = Paths.get(destinationPath, fileName);
+        Files.copy(file.getInputStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+        Card card = new Card();
+        card.setName(name);
+        card.setImage("/dist/img/cards/"+imagename+".jpeg");
+        card.setTypeOfDiscount(typeOfDiscount);
+        cardRepository.save(card);
+        Company company = companyRepository.findById(idCompany).orElseThrow();
+        List<Card> cards = company.getCards();
+        cards.add(card);
+        company.setCards(cards);
+        companyRepository.save(company);
+        return "ok";
+    }
 }
